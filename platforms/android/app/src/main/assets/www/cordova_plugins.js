b3ceeccb491e4ea7dd50ace8eff63da20e449813
bigreni <santhanakrishnan.sai@gmail.com>
2022-04-17 00:32:21 -0500

    fix 2

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -93,7 +93,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-dialogs": "2.0.2",
     "cordova-plugin-device": "2.0.3",
     "cordova-plugin-geolocation": "4.1.0",
-    "cordova-plugin-whitelist": "1.3.4",
     "cordova-plugin-nativestorage": "2.3.2",
     "cordova-plugin-apprate": "1.7.2"
   };

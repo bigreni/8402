3f4bc87b2626acb1716e9911977fe0ec4262ee75
bigreni <santhanakrishnan.sai@gmail.com>
2022-04-16 23:49:47 -0500

    Version 2

---
+++ www/js/main.js
@@ -0,0 +1,65 @@
+    function onLoad() {
+        if ((/(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent))) {
+            document.addEventListener('deviceready', initApp, false);
+        } else {
+            initApp();
+        }
+    }
+    var admobid = {};
+    if (/(android)/i.test(navigator.userAgent)) {
+        admobid = { // for Android
+            banner: 'ca-app-pub-1683858134373419/7790106682',
+            interstitial:'ca-app-pub-9249695405712287/3927718795'
+        };
+    } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) { // for ios
+    admobid = {
+      banner: 'ca-app-pub-1683858134373419/7790106682', 
+      interstitial: 'ca-app-pub-9249695405712287/5312118508'
+    };
+  }
+  
+    function initApp() {
+        if (!AdMob) { alert('admob plugin not ready'); return; }
+        initAd();
+        //display interstitial at startup
+        loadInterstitial();
+    }
+    function initAd() {
+        var defaultOptions = {
+            position: AdMob.AD_POSITION.BOTTOM_CENTER,
+            bgColor: 'black', // color name, or '#RRGGBB'
+            isTesting: false // set to true, to receiving test ad for testing purpose
+        };
+        AdMob.setOptions(defaultOptions);
+        registerAdEvents();
+    }
+    // optional, in case respond to events or handle error
+    function registerAdEvents() {
+        // new events, with variable to differentiate: adNetwork, adType, adEvent
+        document.addEventListener('onAdFailLoad', function (data) {
+            this.restart;     
+        });
+        document.addEventListener('onAdLoaded', function (data) { });
+        document.addEventListener('onAdPresent', function (data) { });
+        document.addEventListener('onAdLeaveApp', function (data) { });
+        document.addEventListener('onAdDismiss', function (data) { 
+            this.restart;     
+        });
+    }
+  
+    function createSelectedBanner() {
+          AdMob.createBanner({adId:admobid.banner});
+    }
+  
+    function loadInterstitial() {
+        if ((/(android|windows phone)/i.test(navigator.userAgent))) {
+            AdMob.prepareInterstitial({ adId: admobid.interstitial, isTesting: true, autoShow: true });
+            //document.getElementById("screen").style.display = 'none';     
+        } else if ((/(ipad|iphone|ipod)/i.test(navigator.userAgent))) {
+            AdMob.prepareInterstitial({ adId: admobid.interstitial, isTesting: true, autoShow: true });
+            //document.getElementById("screen").style.display = 'none';     
+        } else
+        {
+            document.getElementById("screen").style.display = 'none';     
+      }
+    }
